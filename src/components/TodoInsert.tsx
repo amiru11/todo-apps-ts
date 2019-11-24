4ddf0cbf09212e73f32f6de0d7d80ea4625b3430
Saemii <wrfg12@gmail.com>
2019-11-24 18:00:19 +0900

    update :0 HandleChange in Insert with reducer

--- src/components/TodoInsert.tsx
+++ src/components/TodoInsert.tsx
@@ -1,10 +1,40 @@
-import React from "react";
+import React, { useReducer, useCallback } from "react";
 import { MdAdd } from "react-icons/md";
 
+type StateType = {
+  value: string;
+};
+
+function inputReducer(state: StateType, action) {
+  console.log("state", state);
+  return {
+    ...state,
+    [action.name]: action.value
+  };
+}
+
 function TodoInsert(): JSX.Element {
+  const [state, dispatch] = useReducer(inputReducer, {
+    value: ""
+  });
+
+  //
+  const { value } = state;
+
+  const handleChange = useCallback(
+    ({ target }: React.ChangeEvent<HTMLInputElement>): void => dispatch(target),
+    []
+  );
+
   return (
     <form className="TodoInsert">
-      <input type="text" placeholder="할 일을 입력하세요" />
+      <input
+        type="text"
+        placeholder="할 일을 입력하세요"
+        onChange={handleChange}
+        name="value"
+        value={value}
+      />
       <button type="submit">
         <MdAdd />
       </button>
