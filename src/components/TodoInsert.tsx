28a015cc998e7fa3c6b093312ecbd0acab7d3f19
Saemii <wrfg12@gmail.com>
2019-11-27 09:55:12 +0900

    update :0 TodoInsert styling

--- src/components/TodoInsert.tsx
+++ src/components/TodoInsert.tsx
@@ -1,5 +1,6 @@
 import React, { useReducer, useCallback } from "react";
 import { MdAdd } from "react-icons/md";
+import { InsertForm, InsertInput, InsertButton } from "../styles/TodoInsert";
 
 interface ITodoInsertProps {
   onInsert: (value: string) => void;
@@ -42,18 +43,18 @@ function TodoInsert({ onInsert }: ITodoInsertProps): JSX.Element {
   );
 
   return (
-    <form className="TodoInsert" onSubmit={onSubmit}>
-      <input
+    <InsertForm onSubmit={onSubmit}>
+      <InsertInput
         type="text"
         placeholder="할 일을 입력하세요"
         onChange={handleChange}
         name="value"
         value={value}
       />
-      <button type="submit">
+      <InsertButton>
         <MdAdd />
-      </button>
-    </form>
+      </InsertButton>
+    </InsertForm>
   );
 }
 
---
+++ src/styles/TodoInsert.ts
@@ -0,0 +1,38 @@
+import styled from "styled-components";
+
+export const InsertForm = styled.form`
+  display: flex;
+  background: #495057;
+`;
+
+export const InsertInput = styled.input`
+  background: none;
+  outline: none;
+  border: none;
+  padding: 0.5rem;
+  font-size: 1.125rem;
+  line-height: 1.5;
+  color: white;
+  flex: 1;
+  &:placeholder {
+    color: #dee2e6;
+  }
+`;
+
+export const InsertButton = styled.button`
+  background: none;
+  outline: none;
+  border: none;
+  background: #868e96;
+  color: white;
+  padding-left: 1rem;
+  padding-right: 1rem;
+  font-size: 1.5rem;
+  display: flex;
+  align-items: center;
+  cursor: pointer;
+  transition: 0.1s background ease-in;
+  &:hover {
+    background: #adb5bd;
+  }
+`;
